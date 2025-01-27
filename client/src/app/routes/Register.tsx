15c03153e046bdc17f71117cc735ca00f1272f4e
Neil <mallarineil9@gmail.com>
2025-01-27 14:03:41 +0800

    Add conditional rendering for user registration page

--- client/src/app/routes/Register.tsx
+++ client/src/app/routes/Register.tsx
@@ -35,74 +35,98 @@ const Register = () => {
       },
     });
   };
-  return (
-    <div className='flex flex-col justify-center items-center h-screen gap-4'>
-      <h1 className='text-3xl font-bold'>Register</h1>
-      <form
-        onSubmit={handleSubmit(onFormSubmit)}
-        className='flex flex-col gap-5'
-      >
-        <label
-          htmlFor='email'
-          className='text-base flex flex-col gap-1'
+  if (mutation.isPending) {
+    return (
+      <div className='flex h-screen items-center justify-center'>
+        <p
+          role='alert'
+          className='text-lg'
         >
-          Email
-          <input
-            {...register('email')}
-            type='email'
-            name='email'
-            id='email'
-            placeholder='Enter your email'
-            className='text-lg outline-1 outline-gray-400 rounded-sm w-80 p-1.5 border-2 border-transparent focus:border-blue-300'
-          />
-          <ErrorMessage
-            errors={errors}
-            name='email'
-            render={() => (
-              <p
-                role='alert'
-                className='text-red-800 text-base'
-              >
-                {errors.email?.message}
-              </p>
-            )}
-          />
-        </label>
-        <label
-          htmlFor='password'
-          className='text-base flex flex-col gap-1'
+          Registering user...
+        </p>
+      </div>
+    );
+  } else if (mutation.isError) {
+    return (
+      <div className='flex h-screen items-center justify-center'>
+        <p
+          role='alert'
+          className='text-lg text-red-800'
         >
-          Password
-          <input
-            {...register('password')}
-            type='password'
-            name='password'
-            id='password'
-            placeholder='Enter your password'
-            className='text-lg outline-1 outline-gray-400 rounded-sm w-80 p-1.5 border-2 border-transparent focus:border-blue-300'
-          />
-          <ErrorMessage
-            errors={errors}
-            name='password'
-            render={() => (
-              <p
-                role='alert'
-                className='text-red-800 text-base'
-              >
-                {errors.password?.message}
-              </p>
-            )}
-          />
-        </label>
-        <button
-          type='submit'
-          className='w-80 text-lg self-center p-2 bg-blue-500 text-white rounded-sm hover:bg-blue-600 hover:cursor-pointer'
+          We encountered problem registering user...
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div className='flex flex-col justify-center items-center h-screen gap-4'>
+        <h1 className='text-3xl font-bold'>Register</h1>
+        <form
+          onSubmit={handleSubmit(onFormSubmit)}
+          className='flex flex-col gap-5'
         >
-          Register
-        </button>
-      </form>
-    </div>
-  );
+          <label
+            htmlFor='email'
+            className='text-base flex flex-col gap-1'
+          >
+            Email
+            <input
+              {...register('email')}
+              type='email'
+              name='email'
+              id='email'
+              placeholder='Enter your email'
+              className='text-lg outline-1 outline-gray-400 rounded-sm w-80 p-1.5 border-2 border-transparent focus:border-blue-300'
+            />
+            <ErrorMessage
+              errors={errors}
+              name='email'
+              render={() => (
+                <p
+                  role='alert'
+                  className='text-red-800 text-base'
+                >
+                  {errors.email?.message}
+                </p>
+              )}
+            />
+          </label>
+          <label
+            htmlFor='password'
+            className='text-base flex flex-col gap-1'
+          >
+            Password
+            <input
+              {...register('password')}
+              type='password'
+              name='password'
+              id='password'
+              placeholder='Enter your password'
+              className='text-lg outline-1 outline-gray-400 rounded-sm w-80 p-1.5 border-2 border-transparent focus:border-blue-300'
+            />
+            <ErrorMessage
+              errors={errors}
+              name='password'
+              render={() => (
+                <p
+                  role='alert'
+                  className='text-red-800 text-base'
+                >
+                  {errors.password?.message}
+                </p>
+              )}
+            />
+          </label>
+          <button
+            type='submit'
+            className='w-80 text-lg self-center p-2 bg-blue-500 text-white rounded-sm hover:bg-blue-600 hover:cursor-pointer'
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    );
+  }
 };
 
 export default Register;
