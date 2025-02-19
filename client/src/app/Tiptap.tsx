b2b01cbf39c87435d486da8cca036a177e72b0ab
Neil <mallarineil9@gmail.com>
2025-02-19 18:48:13 +0800

    Toggle editor menu visibility conditionally
    Show menu if cursor is in note title or hide otherwise

--- client/src/app/Tiptap.tsx
+++ client/src/app/Tiptap.tsx
@@ -48,6 +48,14 @@ const Tiptap = ({ refetchTitles, updateNoteId, activeNote }: TiptapProps) => {
     },
     editorProps: {
       handleKeyDown: (_view, event) => {
+        const editorElem = document.querySelector('.tiptap');
+
+        // Toggles show menu when user presses "Enter" key at the end of note title
+        // and "Backspace" key at the start of node below note title
+        if (event.target === editorElem) {
+          setShowMenu(!showMenu);
+        }
+
         // this would remove all marks when user presses Enter at the end of a line
         if (event.key === 'Enter') {
           // editor.commands.unsetAllMarks() doesn't work as expected
